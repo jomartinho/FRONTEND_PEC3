5121b759476feb39d828cf08545807a37fa7a7c2
joao rebordao martinho <jmartinho@uoc.edu>
2024-11-20 15:25:32 +0000

    nuevo readme ej4

---
+++ PEC3_Ej4/README_PEC4_Ej4.md
@@ -0,0 +1,13 @@
+# PEC3 Ejercicio 4: Aplicación TODO en TypeScript
+
+## (g) Comandos para ejecutar la aplicación
+
+1. Transpilar los archivos TypeScript a JavaScript:
+   ```bash
+    tsc
+
+    node ./dist/app.js
+    
+    npm install webpack webpack-cli ts-loader --save-dev
+    
+    npx webpack
--- PEC3_Ej4/app.ts
+++ PEC3_Ej4/app.ts
@@ -3,4 +3,11 @@ import { TodoService } from './services/todo.service';
 import { TodoView } from './views/todo.views';
 
 
-const app = new TodoController(new TodoService(), new TodoView());
+const service = new TodoService();
+const view = new TodoView();
+const controller = new TodoController(service, view);
+
+controller.addTodo('Learn TypeScript');
+controller.addTodo('Build a TODO app');
+controller.toggleTodo('1');
+controller.deleteTodo('1');
