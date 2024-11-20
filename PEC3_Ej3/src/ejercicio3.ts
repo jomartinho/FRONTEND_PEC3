1dce333a48be975f298080066ee287c45d656f1a
joao rebordao martinho <jmartinho@uoc.edu>
2024-11-20 14:50:51 +0000

    commiting ejercicio 3-3

---
+++ PEC3_Ej3/src/ejercicio3.ts
@@ -0,0 +1,61 @@
+//Sustituye /***/ por las instrucciones adecuadas que cumplan las operaciones 
+//y salidas indicadas en los comentarios.
+
+abstract class Animal {
+    static population: number = 0;
+    constructor() {
+        Animal.population++;
+    }
+    public abstract sound(): void;
+}
+
+class Dog extends Animal {
+    color: string;
+
+    constructor(color: string) {
+        super();
+        this.color = color;
+      }
+
+    public sound(): void {
+    console.log("WOW");
+    }
+
+    public iamadog() {
+        console.log('yes, this is a dog');
+    }
+}
+
+class Cat extends Animal {
+    gender: string;
+    
+    constructor(gender: string) {
+        super();
+        this.gender = gender;
+      }
+    
+    public sound(): void {
+    console.log("MEOW");
+    }
+
+    public iamacat() {
+        console.log('yes, this is a cat');
+    }
+}
+
+let animals: Animal[] = [];
+animals.push(new Cat('male'));
+animals.push(new Dog('white'));
+animals.push(new Cat('female'));
+animals.push(new Dog('black'));
+
+for(let animal of animals){
+    animal.sound();
+    if (animal instanceof Cat) {
+      (animal as Cat).iamacat();
+    } else if (animal instanceof Dog) {
+      (animal as Dog).iamadog();
+    }
+}
+
+console.log(Animal.population); //4
